de071640f16d2258352b5d938c7d6375907faa9c
Anthony Dominguez <46823001+Anthony-Dominguez@users.noreply.github.com>
2024-01-17 15:21:51 -0500

    Create Ch1Q3.java

---
+++ CPS1231/Homework/Ch1/Ch1Q3.java
@@ -0,0 +1,12 @@
+public class Ch1Q3 {
+    public static void main(String[] args) {
+        System.out.println("Anthony Dominguez, 01,17,2024");
+        System.out.println("");
+
+        double x = 4*(1-1.0/3+1.0/5-1.0/7+1.0/9-1.0/11);
+        double y = 4*(1-1.0/3+1.0/5-1.0/7+1.0/9-1.0/11+1.0/13);
+        System.out.println(
+                "The result of the first problem is : "+ x +
+                "\nThe result of the second problem is : "+y);
+    }
+}
